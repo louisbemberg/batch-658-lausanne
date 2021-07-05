90ec98764ae7b8b2838b8f6fd48ca1746b614643
louis bemberg <louis.bemberg@gmail.com>
2021-07-05 16:26:10 +0200

    finished day 1 reboot

--- reboot-jour-1/3-instacart/instacart_v3.rb
+++ reboot-jour-1/3-instacart/instacart_v3.rb
@@ -1,9 +1,9 @@
 # remodeler le magasin pour inclure prix ET stock (nested hashes)
 store = {
-  "kiwi" => 1.25,
-  "banana" => 0.5,
-  "mango" => 4,
-  "asparagus" => 9
+  'kiwi' => { 'prix' => 1.25, 'stock' => 40 },
+  'banana' => { 'prix' => 0.5, 'stock' => 12 },
+  'mango' => { 'prix' => 4, 'stock' => 20 },
+  'asparagus' => { 'prix' => 9, 'stock' => 150 }
 }
 
 shopping_cart = Hash.new(0)
@@ -16,9 +16,9 @@ item_chosen = nil
 
 until item_chosen == 'quit'
   puts "In our store today:"
-  store.each do |key, value|
+  store.each do |key, nested_hash|
     # rajouter information sur le stock des aliments
-    puts "#{key}: #{value}€"
+    puts "#{key}: #{nested_hash['prix']}€ (#{nested_hash['stock']} en stock)"
   end
 
   puts "Which item? (or 'quit' to checkout)"
@@ -28,11 +28,16 @@ until item_chosen == 'quit'
     puts "How many?"
     number_chosen = gets.chomp.to_i
     # check si il en reste suffisamment
+    if number_chosen <= store[item_chosen]['stock']
       # si oui:
       shopping_cart[item_chosen] += number_chosen
       # retirer du stock au store
+      store[item_chosen]['stock'] -= number_chosen
       #si non
+    else
       # message qui dit qu'on en a pas assez => dire la quantité dispo
+      puts "Sorry, we only have #{store[item_chosen]['stock']} #{item_chosen}(s) in stock"
+    end
   else
     puts "Sorry, we don't have #{item_chosen}" unless item_chosen == 'quit'
   end
@@ -43,7 +48,7 @@ sum = 0
 puts "-------TOTAL------"
 
 shopping_cart.each do |element, quantity|
-  prix_de_un_element = store[element]
+  prix_de_un_element = store[element]['prix']
   prix_de_tous = prix_de_un_element * quantity
   sum += prix_de_tous
   puts "#{element}: #{quantity} X #{prix_de_un_element} = #{prix_de_tous}CHF"
