d73adf2e91e1079ade23bff6f34f5fa08620a34b
louis bemberg <louis.bemberg@gmail.com>
2021-07-05 10:06:17 +0200

    created rebboot day 1 folder

---
+++ reboot-jour-1/1-simple_calculator/calculator_pseudocode.rb
@@ -0,0 +1,19 @@
+# LOOP COMMENCE ICI
+
+#   demander un nombre a l'utilisateur, sauvegarder dans une variable .to_i
+
+#   demander un 2e nombre a l'utilisateur, sauvegarder dans une variable .to_i
+
+#   demander un opérateur a l'utilisateur, sauvegarder dans une variable
+
+#   if statement dépendant de l'opérateur
+#     * si l'utilsateur a écrit "+", fais une addition de var1 et var2
+#     * ...
+#     * ...
+#     * ...
+
+#   montrer la réponse a l'utilisateur
+
+#   demander si on veut calculer de nouveau ou pas? Y/N
+
+# LOOP FINIT ICI
