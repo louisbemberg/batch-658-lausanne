b8e3eea28dad0a96e7fe3d07056a6daede2cb249
louis bemberg <louis.bemberg@gmail.com>
2021-07-05 13:54:05 +0200

    pseudocode step 1 done

---
+++ reboot-jour-1/3-instacart/instacart_v1.rb
@@ -0,0 +1,25 @@
+# créer une (constante) hash du magasin (prix, article)
+# créer un shopping cart / panuier (array?)
+# WELCOME MESSAGE
+
+# créer variable qui permet d'entrer dans le loop (nil)
+# LOOP COMMENCE ICI "jusqu'à ce qu'on me dise 'quit'"
+#   message pour dire ce qu'il y a dans le store
+
+#   itérer sur le hash pour montrer son contenu
+
+#   demander quel item le user veut acheter, sauver dans variable
+
+#   vérifier que l'item choisi est dans le magasin
+#   si oui
+#     ajouter element au panier
+#   si non
+#     message sorry we don't have xxx
+# LOOP FINIT ICI
+
+# déclarer un somme de départ de 0
+# itérer sur notre panier
+#   trouver le prix de chaque élément
+#   ajouter le prix de l'élément à notre somme
+
+# display / montrer le total
